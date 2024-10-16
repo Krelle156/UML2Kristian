de4b088bbcad36c66173b9c7a8c657487c1fe488
Krelle156 <kotofteng@gmail.com>
2024-10-16 17:39:19 +0200

    Regions for MenuItem and tested the use of \ + t

--- PizzaLibrary/Models/MenuItem.cs
+++ PizzaLibrary/Models/MenuItem.cs
@@ -9,9 +9,12 @@ namespace PizzaLibrary.Models
 {
     public class MenuItem : IMenuItem
     {
+        #region Instance Fields
         private static int counter;
         private int _no;
+        #endregion
 
+        #region Constructors
         public MenuItem(string name, double price, string description, MenuType menuType)
         {
             counter++;
@@ -22,17 +25,23 @@ namespace PizzaLibrary.Models
             Description = description;
             TheMenuType = menuType;
         }
+        #endregion
+
+        #region Properties
         public string Description { get; set; }
         public string Name { get; set; }
 
         public int No { get { return _no; } }
         public double Price {  get; set; }
         public MenuType TheMenuType { get; set; }
+        #endregion
 
+        #region Methods
         public override string ToString()
         {
-            return $"Nr. {_no}: {Name}      {TheMenuType}" +
-                $"\n{Description}        {Price}";
+            return $"Nr. {_no}: {Name}\t\t\t{TheMenuType}" +
+                $"\n{Description}\t\t\t{Price}-";
         }
+        #endregion
     }
 }
